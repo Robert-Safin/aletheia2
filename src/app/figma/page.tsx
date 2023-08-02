36d8a2683488f626e27858a073eab250d5fca846
Robert Safin <robertsafin98@gmail.com>
2023-08-02 15:54:16 +0300

    re

--- src/app/figma/page.tsx
+++ src/app/figma/page.tsx
@@ -9,34 +9,20 @@ const FigrmaComponents = async () => {
     <ContainerGray>
 
       <h1 className="main-header">main header</h1>
-      <br />
       <h2 className="secondary-header">secondary header</h2>
-      <br />
       <p className="paragraph">paragraph</p>
-      <br />
       <p className="small-text">small text</p>
-      <br />
       <AiTwotoneStar className="icon-large"/>
-      <br />
       <AiTwotoneStar className="icon-small"/>
-      <br />
 
       <p>primary wide button</p>
       <button className="btn-primary-wide">BUTTON</button>
-      <br />
-      <br />
       <p>secondary wide button</p>
       <button className="btn-secondary-wide">BUTTON</button>
-      <br />
-      <br />
       <p>primary small button</p>
       <button className="btn-primary-small">BUTTON</button>
-      <br />
-      <br />
       <p>secondary small button</p>
       <button className="btn-secondary-small">BUTTON</button>
-      <br />
-      <br />
 
 
 
